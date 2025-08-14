56d0b284c9961f9209b338d112c979584e9dec4a
TahraouiMustapha <moustaphaoussama1818@gmail.com>
2025-08-14 22:54:05 +0100

    install passport express-session prisma-session and other dependencies to authenticate

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
 const express = require("express")
+const path = require("path")
+const expressSession = require("express-session")
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store")
+const { PrismaClient } = require("@prisma/client")
 const app = express()
 require("dotenv").config()
 
-app.get("/", (req, res)=> res.send("hi"))
+// views config
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+// sessions config
+app.use(
+    expressSession({
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        }, 
+        secret: 'my secret', 
+        resave: true, 
+        saveUninitialized: true, 
+        store: new PrismaSessionStore(
+            new PrismaClient(), 
+        {
+            checkPeriod: 2 * 60 * 1000,  
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }
+        )
+    })
+)
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res)=> res.render("main"))
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, ()=> {
+app.listen(PORT, (err)=> {
+    if(err) {
+        throw err;
+    }  
     console.log(`listening on Port : ${PORT}`)
 })
